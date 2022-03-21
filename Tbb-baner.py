502f901f4f42f5d07548cb8b59c0a42239d31138
Team-Back-Berry <102026469+Team-Back-Berry@users.noreply.github.com>
2022-03-21 21:26:48 +0600

    Add files via upload

---
+++ Tbb-baner.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import sys
+
+os.system('apt update')
+os.system('apt upgrade -y')
+os.system('pkg install figlet -y')
+os.system('pkg install ncurses-utils -y') 
+os.system('pkg install ruby -y')
+os.system('gem install lolcat')
+
+output = '/data/data/com.termux/files/usr/etc/'
+
+print('')
+name = raw_input('Input your Name : ')
+
+wlc = '''
+import os,sys,time,random
+print("")
+print("")
+color = ["\\033[1;31m","\\033[1;32m"]
+m = random.choice(color)+"                   Welcome {} \\n"
+for msg in m:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    time.sleep(0.06)
+print("")
+'''.format(name)
+
+h1 = open(output+'wlc.py', 'w')
+h1.write(wlc)
+h1.close()
+
+bashrc1 = '''
+clear
+echo
+echo "
+       𒆜� ⌐■-■ ★ CREATED BY  ALAMIN⌐■-■ �𒆜 " |lolcat
+echo
+    
+    echo "        We Are TEAM BLACK BERRY " |lolcat
+'''
+
+bashrc2 = '''
+echo "
+         <<<<<<< We Work For Dignity >>>>>>>
+_____________________Not For Money___________________
+                              
+      一━══̷'デ︻▄ ⭐🎭 T.B.B🎭⭐ ▄︻デ,̷══━一" |lolcat
+python /data/data/com.termux/files/usr/etc/wlc.py
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+                /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        }
+fi
+#PS1="\\033[1;31mA.R.E~>>"
+PS1="\[\e[1;34m┌──\\a─T─I─M─E─\\a──┐\\033[1;34m\\a┌──\\a─D─A─T─E─\\a───>\\033[1;34m
+\\a┌─[\\033[1;93m \@\\033[1;34m ]──[\\033[1;93m \d\\033[1;34m ]\\033[1;34m
+\\a├─[\\033[1;32m\w\\033[1;34m]\\033[1;34m
+'''
+
+h2 = open(output+'bash.bashrc', 'w')
+h2.write(bashrc1)
+h2.write("\nfiglet    '    "+name+"' |lolcat\n")
+h2.write(bashrc2)
+h2.write('\[\e[34m\]└─>\[\e[35m\]'+name+'\[\e[34m\]>>>>\[\e[1;32m\] "\n')
+h2.write('echo -e "\e[6 q"')
+h2.close()
+print('DONE')
